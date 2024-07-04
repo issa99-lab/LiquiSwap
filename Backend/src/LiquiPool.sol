36e957ba78445c972f9d0e6b321e7a5081c520d1
issa99-lab <issanana99@gmail.com>
2024-07-04 13:34:27 +0300

    Pool factory created

---
+++ Backend/src/LiquiPool.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GNU General Public License v3.0
+pragma solidity ^0.8.18;
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LiquiPool is ERC20 {
+    using SafeERC20 for IERC20;
+
+    //State variables
+    IERC20 private immutable i_wethToken;
+    IERC20 private immutable i_token;
+
+    constructor(
+        address _weth,
+        address _token,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        i_wethToken = _weth;
+        i_token = _token;
+    }
+}
---
+++ Backend/src/PoolFactory.sol
@@ -0,0 +1,79 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {LiquiPool} from "src/LiquiPool.sol";
+import {IERC20} from "lib/forge-std/src/interfaces/IERC20.sol";
+
+contract PoolFactory {
+    //-----ERRORS-----//
+    error ERR_PoolExists(address);
+    error ERR_AdminOnlyCall(address);
+
+    //-----STATE VARIABLES----//
+    mapping(address token => address pool) private s_pools;
+    mapping(address pool => address token) private s_tokens;
+
+    address private immutable i_wethToken;
+    address private poolOwner;
+
+    //-----EVENTS-----//
+    event PoolCreated(address tokenAddress, address pool);
+
+    constructor(address _weth) {
+        i_wethToken = _weth;
+        poolOwner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != poolOwner) {
+            revert ERR_AdminOnlyCall(msg.sender);
+        }
+        _;
+    }
+
+    //-----FUNCTIONS-----//
+    //@dev creates a new pool of token address that is passed
+    function createPool(address token) external onlyOwner returns (address) {
+        if (s_pools[token] != address(0)) {
+            revert ERR_PoolExists(token);
+        }
+
+        string memory tokenName = string.concat("Liqui", IERC20(token).name());
+        string memory tokenSymbol = string.concat(
+            "Liqui",
+            IERC20(token).symbol()
+        );
+
+        LiquiPool pool = new Liquipool(
+            i_wethToken,
+            token,
+            tokenName,
+            tokenSymbol
+        );
+        s_pools[token] = address(pool);
+        s_tokens[address(pool)] = token;
+        emit PoolCreated(token, address(pool));
+        return address(pool);
+    }
+
+    //
+    function setNewOwner(
+        address newOwner
+    ) external onlyOwner returns (address) {
+        poolOwner = newOwner;
+        return newOwner;
+    }
+
+    //-----GETTERS-----//
+    function getPoolAddress(address token) external view returns (address) {
+        return s_pools[token];
+    }
+
+    function getTokenAddress(address pool) external view returns (address) {
+        return s_tokens[pool];
+    }
+
+    function getWethToken() external view returns (address) {
+        return i_wethToken;
+    }
+}
